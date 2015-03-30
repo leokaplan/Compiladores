b08fbdd1d3a08cc9e63c6e6a633a1c30822e7159
Bernardo Alkmim <bpalkmim@gmail.com>
2015-03-29 21:10:36 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 Trabalho de Compiladores INF1715
 ----------------------------------
 Bernardo Pinto de Alkmim - 1210514
-Leonardo Kaplan - 1xxxxxx
+Leonardo Kaplan - 1212509
 
 Arquivo de testes do analisador léxico
 */
