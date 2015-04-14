a73e3754a58a2b1efbe01a0bb20c2ec1390e6950
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2015-04-14 14:01:01 -0300

    testes

--- build.sh
+++ build.sh
@@ -1,12 +1,24 @@
 #! /bin/bash
 make
-for i in testes/*.in
+echo "===== testando lexico"
+for i in testes_lex/*.in
 do
     filename=$(basename "$i")
     filename="${filename%.*}"
     echo "=== testando $filename"
-    ./main < testes/$filename.in > testes/$filename.out
-    diff testes/$filename.out testes/$filename.gab
+    ./main < testes_lex/$filename.in > testes_lex/$filename.out
+    diff testes_lex/$filename.out testes_lex/$filename.gab
+    echo "=== acabou   $filename"
+
+done
+echo "===== testando sintatico"
+for i in testes_sin/*.in
+do
+    filename=$(basename "$i")
+    filename="${filename%.*}"
+    echo "=== testando $filename"
+    ./main < testes_sin/$filename.in > testes_sin/$filename.out
+    diff testes_sin/$filename.out testes_sin/$filename.gab
     echo "=== acabou   $filename"
 
 done
