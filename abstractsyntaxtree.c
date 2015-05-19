0d1ba3b7d7f590f9b1a5a505c87738a2ed14cff1
Leonardo Kaplan <contato.leonardokaplan@gmail.com>
2015-05-19 15:28:31 -0300

    macros, macros everywhere

--- abstractsyntaxtree.c
+++ abstractsyntaxtree.c
@@ -156,14 +156,14 @@ AST_nodeType * AST_litString(char * value) {
 
 AST_nodeType * AST_id(char * name) {
 	AST_nodeType * p;
-        MAKE_NODE(p,TYPE_ID,ID);
+    MAKE_NODE(p,TYPE_ID,ID);
 	p->node.id.name = strdup(name);
 	return p;
 }
 
 AST_nodeType * AST_type(AST_typeEnum type, int indirections) {
 	AST_nodeType * p;
-        MAKE_NODE(p,TYPE_TYP,TYP);
+    MAKE_NODE(p,TYPE_TYP,TYP);
 
 	p->node.typ.type = type;
 	p->node.typ.indirections = indirections;
@@ -175,13 +175,13 @@ AST_nodeType * AST_exp_opr(int oper, AST_nodeType * exp1, AST_nodeType * exp2) {
 	AST_nodeType * p;
 	if (exp2 == NULL)
         {
-            MAKE_NODE(p,TYPE_EXP,UNOP);
+            MAKE_NODE(p,TYPE_EXP,EXP_UNOP);
         }
         else
         {
-            MAKE_NODE(p,TYPE_EXP,BINOP);
+            MAKE_NODE(p,TYPE_EXP,EXP_BINOP);
         }
-        p->node.exp.operexp.opr = oper;
+    p->node.exp.operexp.opr = oper;
 	p->node.exp.operexp.exp1 = exp1;
 	p->node.exp.operexp.exp2 = exp2;
 
@@ -189,9 +189,9 @@ AST_nodeType * AST_exp_opr(int oper, AST_nodeType * exp1, AST_nodeType * exp2) {
 }
 AST_nodeType * AST_exp_new(AST_nodeType * type, AST_nodeType * exp){
 	AST_nodeType * p;
-        MAKE_NODE(p,TYPE_EXP,EXP_NEW);
+    MAKE_NODE(p,TYPE_EXP,EXP_NEW);
 	
-        p->node.exp.newexp.type = type;
+    p->node.exp.newexp.type = type;
 	p->node.exp.newexp.exp = exp;
 
         return p;
@@ -200,13 +200,7 @@ AST_nodeType * AST_exp_new(AST_nodeType * type, AST_nodeType * exp){
 AST_nodeType * AST_exp_var(AST_nodeType * var){
 	AST_nodeType * p;
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
-	
-	p->type = TYPE_EXP;
-	p->tag = EXP_VAR;
-	p->nextElem = NULL;
-	p->lastElem = p;
+    MAKE_NODE(p,TYPE_EXP,EXP_VAR);
 	p->node.exp.varexp = var;
 
 	return p;
@@ -215,13 +209,7 @@ AST_nodeType * AST_exp_var(AST_nodeType * var){
 AST_nodeType * AST_exp_call(AST_nodeType * exp1, AST_nodeType * exp2){
 	AST_nodeType * p;
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
-	
-	p->type = TYPE_EXP;
-	p->tag = EXP_CALL;
-	p->nextElem = NULL;
-	p->lastElem = p;
+    MAKE_NODE(p,TYPE_EXP,EXP_CALL);
 	p->node.exp.callexp.exp1 = exp1;
 	p->node.exp.callexp.exp2 = exp2;
 
@@ -232,13 +220,7 @@ AST_nodeType * AST_exp_call(AST_nodeType * exp1, AST_nodeType * exp2){
 AST_nodeType * AST_var_array(AST_nodeType * exp1, AST_nodeType * exp2){
 	AST_nodeType * p;
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
-	
-	p->type = TYPE_VAR;
-	p->tag = VAR_ARRAY;
-	p->nextElem = NULL;
-	p->lastElem = p;
+    MAKE_NODE(p,TYPE_VAR,VAR_ARRAY);
 	p->node.var.indexed.exp1 = exp1;
 	p->node.var.indexed.exp2 = exp2;
 
@@ -248,13 +230,7 @@ AST_nodeType * AST_var_array(AST_nodeType * exp1, AST_nodeType * exp2){
 AST_nodeType * AST_var_simple(AST_nodeType * id){
 	AST_nodeType * p;
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
-	
-	p->type = TYPE_VAR;
-	p->tag = VAR_SIMPLE;
-	p->nextElem = NULL;
-	p->lastElem = p;
+    MAKE_NODE(p,TYPE_VAR,VAR_SIMPLE);
 	p->node.var.id = id;
 
 	return p;
@@ -262,13 +238,8 @@ AST_nodeType * AST_var_simple(AST_nodeType * id){
 AST_nodeType * AST_decl_var(AST_nodeType * type, AST_nodeType * id){
 	AST_nodeType * p;
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
+    MAKE_NODE(p,TYPE_DECL,DEC_VAR);
 	
-	p->type = TYPE_DECL;
-	p->tag = DEC_VAR;
-	p->nextElem = NULL;
-	p->lastElem = p;
 	p->node.decl.vardecl.type = type;
 	p->node.decl.vardecl.id = id;
 
@@ -276,14 +247,8 @@ AST_nodeType * AST_decl_var(AST_nodeType * type, AST_nodeType * id){
 }
 AST_nodeType * AST_decl_func(AST_nodeType * type, AST_nodeType * id, AST_nodeType * param, AST_nodeType * block){
 	AST_nodeType * p;
+    MAKE_NODE(p,TYPE_DECL,DEC_FUNC);
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
-	
-	p->type = TYPE_DECL;
-	p->tag = DEC_FUNC;
-	p->nextElem = NULL;
-	p->lastElem = p;
 	p->node.decl.funcdecl.type = type;
 	p->node.decl.funcdecl.id = id;
 	p->node.decl.funcdecl.param = param;
@@ -296,13 +261,7 @@ AST_nodeType * AST_decl_func(AST_nodeType * type, AST_nodeType * id, AST_nodeTyp
 AST_nodeType * AST_cmd_if(AST_nodeType * exp, AST_nodeType * cmd1, AST_nodeType * cmd2) {
 	AST_nodeType * p;
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
-
-	p->type = TYPE_CMD;
-	p->tag = CMD_IF;
-	p->nextElem = NULL;
-	p->lastElem = p;
+    MAKE_NODE(p,TYPE_CMD,CMD_IF);
 	p->node.cmd.ifcmd.exp = exp;
 	p->node.cmd.ifcmd.cmd1 = cmd1;
 	p->node.cmd.ifcmd.cmd2 = cmd2;	
@@ -313,13 +272,8 @@ AST_nodeType * AST_cmd_if(AST_nodeType * exp, AST_nodeType * cmd1, AST_nodeType 
 AST_nodeType * AST_cmd_while(AST_nodeType * exp, AST_nodeType * cmd) {
 	AST_nodeType * p;
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
+    MAKE_NODE(p,TYPE_CMD,CMD_WHILE);
 
-	p->type = TYPE_CMD;
-	p->tag = CMD_WHILE;
-	p->nextElem = NULL;
-	p->lastElem = p;
 	p->node.cmd.whilecmd.exp = exp;
 	p->node.cmd.whilecmd.cmd = cmd;
 
@@ -328,14 +282,8 @@ AST_nodeType * AST_cmd_while(AST_nodeType * exp, AST_nodeType * cmd) {
 
 AST_nodeType * AST_cmd_attr(AST_nodeType * var, AST_nodeType * exp) {
 	AST_nodeType * p;
+    MAKE_NODE(p,TYPE_CMD,CMD_ATTR);
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
-
-	p->type = TYPE_CMD;
-	p->tag = CMD_ATTR;
-	p->nextElem = NULL;
-	p->lastElem = p;
 	p->node.cmd.attrcmd.var = var;
 	p->node.cmd.attrcmd.exp = exp;
 
@@ -345,13 +293,7 @@ AST_nodeType * AST_cmd_attr(AST_nodeType * var, AST_nodeType * exp) {
 AST_nodeType * AST_cmd_ret(AST_nodeType * exp) {
 	AST_nodeType * p;
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
-
-	p->type = TYPE_CMD;
-	p->tag = CMD_RET;
-	p->nextElem = NULL;
-	p->lastElem = p;
+    MAKE_NODE(p,TYPE_CMD,CMD_RET);
 	p->node.cmd.retcmd.exp = exp;
 
 	return p;
@@ -359,27 +301,16 @@ AST_nodeType * AST_cmd_ret(AST_nodeType * exp) {
 AST_nodeType * AST_cmd_exp(AST_nodeType * exp) {
 	AST_nodeType * p;
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
 
-	p->type = TYPE_CMD;
-	p->tag = CMD_EXP;
-	p->nextElem = NULL;
-	p->lastElem = p;
+    MAKE_NODE(p,TYPE_CMD,CMD_EXP);
 	p->node.cmd.expcmd.exp = exp;
 
 	return p;
 }
 AST_nodeType * AST_cmd_block(AST_nodeType * decl, AST_nodeType * cmd){
 	AST_nodeType * p;
+    MAKE_NODE(p,TYPE_CMD,CMD_BLOCK);
 
-	if ((p = (AST_nodeType *) malloc(sizeof(AST_nodeType))) == NULL)
-		yyerror("Falta de memoria");
-
-	p->type = TYPE_CMD;
-	p->tag = CMD_BLOCK;
-	p->nextElem = NULL;
-	p->lastElem = p;
 	p->node.cmd.blockcmd.decl = decl;
 	p->node.cmd.blockcmd.cmd = cmd;
 
@@ -442,7 +373,7 @@ void GRAPH_drawNode(AST_nodeType *p,int ident){
 				}
 				break;
 			case DEC_VAR:
-				printf("")
+				printf("");
                 GRAPH_drawNode(p->node.decl.vardecl.type,ident);
 				printf(" ");
                 GRAPH_drawNode(p->node.decl.vardecl.id,ident);
