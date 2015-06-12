a3ad2345c4746f0a2a24db96b7af5ab749926ca1
Bpalkmim <bpalkmim@gmail.com>
2015-06-12 16:02:03 -0300

    criadas assinaturas para boa parte das funções do módulo

--- assembler.c
+++ assembler.c
@@ -1,7 +1,20 @@
 #include "assembler.h"
+#include <stdio.h>
+#include <string.h>
 
-void generateMachineCode(AST_nodeType * tree) {
+#define OPEN_FILE(file, fileName) \
+            if((file = fopen(fileName, "w")) == NULL) \
+                yyerror("Nao foi possivel abrir o arquivo da linguagem objeto.")
 
-    // TODO
+void ASS_generateMachineCode(AST_nodeType * tree, char * fileName) {
+    FILE * file;
+    OPEN_FILE(file, fileName);
 
+    // TODO talvez criar alguma função auxiliar de adicionar aos labels lá de cima (.globl ou algo assim
+    generateDeclCode(node);
+    
+    // TODO criar uma função auxiliar para escrever o fim do arquivo .s
+    fclose(file);
 }
+
+// TODO colocar os corpos das funções definidas no header
--- assembler.h
+++ assembler.h
@@ -14,7 +14,52 @@
  * Função que gera o arquivo .s de saída a partir da AST.
  *
  * @param tree Árvore de sintaxe abstrata.
+ * @param fileName Nome do arquivo a ser criado.
  */
-void generateMachineCode(AST_nodeType * tree);
+void ASS_generateMachineCode(AST_nodeType * tree, char * fileName);
+
+// Funções internas do módulo
+// Declarações
+static void generateDeclCode(AST_nodeType * node);
+// Subdivisões de declarações
+static void generateDeclVarCode(AST_nodeType * node);
+static void generateDeclFuncCode(AST_nodeType * node);
+
+// TODO Será necessário para tipos? A ver.
+
+// Comandos
+static void generateCmdCode(AST_nodeType * node);
+// Subdivisões de comandos
+static void generateCmdIfCode(AST_nodeType * node);
+static void generateCmdWhileCode(AST_nodeType * node);
+static void generateCmdAttrCode(AST_nodeType * node);
+static void generateCmdRetCode(AST_nodeType * node);
+static void generateCmdExpCode(AST_nodeType * node);
+static void generateCmdBlockCode(AST_nodeType * node);
+
+// Subsubdivisões de comandos
+// TODO
+
+// Expressões
+static void generateExpCode(AST_nodeType * node);
+// Subdivisões de expressões
+static void generateExpOprCode(AST_nodeType * node);
+static void generateExpLitCode(AST_nodeType * node);
+static void generateExpVarCode(AST_nodeType * node);
+static void generateExpParenCode(AST_nodeType * node);
+static void generateExpCallCode(AST_nodeType * node);
+static void generateExpNewCode(AST_nodeType * node);
+
+// Subsubdivisões de expressões
+// TODO
+
+// Variáveis
+static void generateVarCode(AST_nodeType * node);
+// Subdivisões de variáveis
+static void generateVarSimpleCode(AST_nodeType * node);
+static void generateVarArrayCode(AST_nodeType * node);
+
+static void generateIdCode(AST_nodeType * node);
+// TODO Ver como gerar as listas de coisas, provavelmente pelos próprios nós
 
 #endif
